26b8eef5e2b5506fe18b9b2ca62475c6e76ae663
Shail K Shah <anonshail@gmail.com>
2019-10-11 16:32:51 +0530

    added help string

--- help.py
+++ help.py
@@ -3,7 +3,21 @@
 #The help string contains all the information about the syntax and parameter/option usage.
 
 helpString = '''
-Welcome to Web Scraping DSL! This contains all the information regarding the syntax and options usage!
+DESIGN OF SYNTAX
+COMMANDS: 
+All the commands are to be case insensitive. We are working only with single parameters for a particular key.
+1) “Get” command: 
+Syntax: Get “<type_of_file>” from “<URL_NAME>” <optional_command>. <type_of_file> would include 1) text, 2) URL 3) ‘img’ (for images) 4) ‘vid’ for videos 3) File-variations (.‘extension’) 4) Documents and Multimedia (.mp3, .mp4).  This command gets the contents of the URL for the type of file specified by the user.
+Another variation of the command: 
+get extension <Extension_type> from URL <optional_command>.
+2) “View” command: Default syntax: view from <URL>            
+Syntax: view “<type_of_file>” from “<URL>”. Like the metadata operation, the view command is used to look at the contents of the given URL with the type of file to lookup as the parameter.
+3) “Help” command: Syntax: Help. This command is used to give the entire documentation of our web-based domain specific language. This will include information on how to use the various command along with details of the parameters. 
+The following commands are optional parameters.
+4) “Where” command: Syntax: get <command> from <URL> where <constraint1> <constraint2>. Where command is used to list the various constraints while fetching the data. The constraints would be written in regex.
+5) “Store” command: Syntax: store <filename> <directory>. This command is used to store the files fetched to a particular directory.
+6) “Write” command: Syntax: write<text> <filename>. This command is used to write the details into a file.
+
 
 '''
 
