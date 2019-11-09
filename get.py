c5ccf94d03ea2229dbc3b1cd2c88150b290c6d52
anonshail <anonshail@gmail.com>
2019-11-09 12:09:42 +0530

    added get images functionality

--- get.py
+++ get.py
@@ -3,6 +3,11 @@
 from bs4 import BeautifulSoup
 import scrapy
 import requests
+import sys
+import shutil
+import re
+import os.path
+import os
 
 def main(tokList, lineNo):
 
@@ -18,6 +23,7 @@ def main(tokList, lineNo):
 
     #if else ladder of all the get options
     if(tokList[1] == "text"):
+        #cases for text scraping
 
         #error checking, making sure that all the parameters are correct
         for i in range(len(tokList)):
@@ -41,6 +47,72 @@ def main(tokList, lineNo):
         else:
             #display all the text of the page on the terminal
             print(contents)
+
+    elif(tokList[1]=="images"):
+        #case for images
+        storeFlag = False
+        storeLocation = ''   #location to store images into, if storeFlat is set to true
+        
+
+        #error checking, making sure that all the parameters are correct
+        for i in range(len(tokList)):
+            if tokList[i] == 'get' or tokList[i] == 'images' or tokList[i] == 'from' or tokList[i] == 'store':
+                continue
+            elif tokList[i-1] == 'images' or tokList[i-1] == 'from':
+                continue
+            elif tokList[i-1] == 'store':   #if you want to store into a folder
+                storeFlag = True            #enabling store
+                storeLocation = tokList[i]   #location to store is set
+
+                #check if storeLocation exists, or else create it
+                if(os.path.exists(storeLocation)==False):
+                    os.mkdir(storeLocation)
+
+
+            else:
+                print("Incorrect token: " + tokList[i] + " on line no: " + str(lineNo) + ". Execute help for information.")
+                return -1
+
+        #check which images to get, i.e., all or some specific images
+        #here, tokList[2] must contain all, or a particular image to download
+
+        if(tokList[2] == 'all'):
+            #download all images
+            img_tags = soup.find_all('img')
+            urls = [img['src'] for img in img_tags]
+
+            for url in urls:
+                #filename = re.search(r'/([\w_-]+[.](jpg|gif|png))$', url)
+                filename = url.split('/')[-1].split('#')[0].split('?')[0]
+                with open(storeLocation+filename, 'wb') as f:
+                    if 'http' not in url:
+                        # sometimes an image source can be relative 
+                        # if it is provide the base url which also happens 
+                        # to be the site variable atm. 
+                        url = '{}{}'.format(tokList[urlIndex], url)
+                    response = requests.get(url)
+                    f.write(response.content)
+
+        else:
+            #download a particular image
+            if(tokList[2]=='from'):
+                #no image has been mentioned, hence, throw error, while checking for store
+                print("No image mentioned on line no: " + str(lineNo) + ". Execute help for information.")
+                return -1
+            
+            #now, download image stored in tokList[2] or throw an error, while checking for store
+            urlOfFile = tokList[2]
+            filename = urlOfFile.split('/')[-1].split('#')[0].split('?')[0]
+            with open(storeLocation+filename, 'wb') as f:
+                if 'http' not in urlOfFile:
+                    # sometimes an image source can be relative 
+                    # if it is provide the base urlOfFile which also happens 
+                    # to be the site variable atm. 
+                    urlOfFile = '{}{}'.format(tokList[urlIndex], urlOfFile)
+                response = requests.get(urlOfFile)
+                f.write(response.content)
+
+
 
     else:
         print("Unkown parameter: " + tokList[1] + " on line no: " + str(lineNo))
