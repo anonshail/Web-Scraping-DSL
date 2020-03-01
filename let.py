b8d443ac7093cb0e15f6381ff80a0fbc1bc7fe99
Shail <anonshail@gmail.com>
2020-03-01 21:37:00 +0530

    let and list initialized

---
+++ let.py
@@ -0,0 +1,2 @@
+#Let command in web scraping DSL
+#This file is for creating of Variables and returning the name/value pair to main for storage in local variables table
---
+++ list.py
@@ -0,0 +1,2 @@
+#List command in web scraping dsl
+#This file is for creating lists, and it will return a name/list pair to get
